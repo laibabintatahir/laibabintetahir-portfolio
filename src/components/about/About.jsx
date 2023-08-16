a8c090e0b9d71a1617c0e160bd44b652af93c6c2
laiba binta tahir <laibabintatahir@gmail.com>
2023-08-16 15:09:41 +0500

    About Component Completed

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./About.css";
 import AboutImg from "../../assets/p4.jpg";
 import CV from "../../assets/CV.pdf";
+import Info from "./Info";
 
 const About = () => {
   return (
@@ -10,10 +11,18 @@ const About = () => {
       <span className="section__subtitle">My introduction</span>
       <div className="about__container container grid">
         <img src={AboutImg} alt="" className="about__img" />
-
+        <div className="about__data">
+          <Info />
+          <p className="about__description">
+            Frontend Developer, I create web pages with UI/UX user inteface.Passionate front-end developer with 4 independent projects showcasing HTML, CSS, and JavaScript skills.
+          </p>
+          <a download="" href={CV} className="button button--flex">
+            Download CV <i class="uil uil-import"></i>
+          </a>
+        </div>
       </div>
     </section>
   );
-}; 
+};
 
 export default About;
