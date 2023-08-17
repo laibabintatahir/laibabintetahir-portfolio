cb908513988b9f5c520786b938ddaa9434961b18
laiba binta tahir <laibabintatahir@gmail.com>
2023-08-17 21:43:13 +0500

    Qualifications Component..

---
+++ src/components/qualifications/Qualifications.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "./Qualifications.css";
+const Qualifications = () => {
+  return (
+    <sections className="qualification section">
+      <h2 className="section__title">Qualifications</h2>
+      <span className="section__subtitle">My Educational Journey</span>
+    
+    <div className="qualification__container container">
+        <div className="qualification__tab">
+            <div className="qualification__button button--flex">
+                <i className="uil uil- qualification_icon">
+                    
+                </i>
+            </div>
+        </div>
+    </div>
+    </sections>
+  );
+};
+
+export default Qualifications;
