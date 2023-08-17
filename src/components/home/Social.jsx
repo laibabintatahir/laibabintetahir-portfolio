1cb522acf4b3f439c5e3a8f2e787ef3be6564a11
laiba binta tahir <laibabintatahir@gmail.com>
2023-08-17 19:13:13 +0500

    services component

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,16 +3,16 @@ import "./Home.css";
 const Social = () => {
   return (
     <div className="home__social">
-        <a href='https://instagram.com/' className="home__social-icon" target="_blank">
+        <a href='https://instagram.com/_aestheticanaya' className="home__social-icon" target="_blank">
         <i class="uil uil-instagram"></i></a>
 
-        <a href="https://twitter.com/" className="home__social-icon" target="_blank">
+        <a href="https://twitter.com/laibabintatahir" className="home__social-icon" target="_blank">
         <i class="uil uil-twitter"></i></a>
 
-        <a href="https://email.com/"  className="home__social-icon" target="_blank">
+        <a href="https://laibabintatahir@gmail.com/"  className="home__social-icon" target="_blank">
         <i class="uil uil-envelope"></i> </a>
 
-        <a href="https://github.com/"  className="home__social-icon" target="_blank">
+        <a href="https://github.com/laibabintatahir"  className="home__social-icon" target="_blank">
        <i class="uil uil-github-alt"></i></a>
     </div>
   )
