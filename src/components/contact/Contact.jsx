38398204cdeddef6b08f6f3ac03ce7208d7c39c0
Laiba Binta Tahir <111231082+laibabintatahir@users.noreply.github.com>
2024-01-26 11:25:42 +0500

    Update Contact.jsx

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -48,19 +48,6 @@ const Contact = () => {
                 <i className="bx bx-right-arrow-alt contact__button-icon"></i>
               </a>
             </div>
-
-            <div className="contact__cart">
-              <i className="bx bxl-messenger contact__card-icon"></i>
-              <h3 className="contact__card-title">Messanger</h3>
-              <span className="contact__card-data">user.fb123<br/></span>
-              <a
-                href="https://m.me/laibabintatahir"
-                className="contact__button"
-              >
-                Write me
-                <i className="bx bx-right-arrow-alt contact__button-icon"></i>
-              </a>
-            </div>
           </div>
         </div>
         {/* form */}
