376bd9d4791f1082f91c3b3a69161a7a7f1e2e42
Laiba Binta Tahir <111231082+laibabintatahir@users.noreply.github.com>
2024-09-02 13:57:05 +0500

    Update Social.jsx

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -3,7 +3,7 @@ import "./Home.css";
 const Social = () => {
   return (
     <div className="home__social">
-        <a href='https://instagram.com/_aestheticanaya' className="home__social-icon" target="_blank">
+        <a href='https://instagram.com/laibalifelogs' className="home__social-icon" target="_blank">
         <i class="uil uil-instagram"></i></a>
 
         <a href="https://twitter.com/laibabintatahir" className="home__social-icon" target="_blank">
